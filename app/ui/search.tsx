992c385a4f9ddc21c4e3b2d9eff181fc734795c6
Jesse Emamalie <jesseemamalie2@gmail.com>
2024-07-08 10:01:45 -0400

    feat: added pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     const value: string = e.target.value;
 
     if (value) {
